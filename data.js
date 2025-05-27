0d68a70c83591eb004c10ccf4f7b2ff41f8de680
buidathieu <35317761+buidathieu@users.noreply.github.com>
2025-05-27 08:26:47 +0700

    Update data.js

--- data.js
+++ data.js
@@ -104,5 +104,187 @@ window.tuongTacData = [
         "xu_ly": "Liều lượng của aciclovir và valacyclovir có thể cần phải giảm nếu xuất hiện nhiễm độc aciclovir."
       }
     ]
+  },
+  {
+    "hoat_chat": "Acid ascorbic",
+    "mo_ta": "Acid ascorbic và natri ascorbat được chỉ định để dự phòng và điều trị thiếu hụt vitamin C. Thiếu hụt này do thiếu dinh dưỡng và có thể gây bệnh scorbut ở giai đoạn cuối. Acid ascorbic tham gia mạnh vào các phản ứng oxy hóa - khử ở tế bào.",
+    "cac_thuoc_trong_nhom": [
+      "ACID ASCORBIC",
+      "Acti-5",
+      "Aronamin",
+      "Ascormin ống tiêm 500mg/5mL",
+      "Ascortonyl ống uống 5mL (kèm K và Mg aspartat)",
+      "Aspirin C",
+      "Aspirin UPSA-Vitamin C",
+      "ASS+ C Pharmavit",
+      "Ca-C 1000 Sandoz viên sủi",
+      "Calcium Corbière C 1000 DPP",
+      "Calcium Corbière Vitaminé",
+      "Cal-C-vita viên sủi",
+      "Callimon viên sủi",
+      "Cegrovit viên sủi",
+      "Celaskon viên sủi",
+      "Ceelin-thuốc giọt uống 100mg/1mL; siro 100mg/5mL",
+      "Cevita ống tiêm 500mg/5mL",
+      "Cevita ống tiêm 100mg/2mL",
+      "C-Vitamin Pharmavit viên sủi",
+      "Cyclo 3 forte viên",
+      "Effemol gói thuốc",
+      "Fervex cốm",
+      "Gluco C viên nén",
+      "Igatan nang",
+      "Laroscorbine ống tiêm 1000mg/5mL",
+      "Laroscorbine viên sủi 1g",
+      "My vita – vitamin C viên nén sủi 75mg",
+      "Plusssz-vitamin C viên nén sủi 60mg",
+      "Polytonyl enfant dung dịch uống",
+      "Rutin C viên nén",
+      "Soda C gói bột",
+      "Solucetyl viên sủi",
+      "Upsa C viên nén sủi 1 g",
+      "Vita C viên nén 100mg",
+      "Vita-Cal C viên sủi",
+      "Vitamin C dung dịch tiêm 10 % ống 5 mL; 500 mg/5mL",
+      "Vitamin C ống tiêm dung dịch 5 % ống 2 mL",
+      "Vitamin C Oberlin cốm pha dung dịch, gói 1000 mg",
+      "Vitamin C Oberlin viên nén",
+      "Vitamin C viên nén 100 mg; 500 mg",
+      "Vitamin C viên sủi bọt 1000 mg",
+      "Vitamin C Bil dung dịch tiêm",
+      "Ca-C 1000 Sandoz-viên nén sủi",
+      "Calcium Corbière vitamin ống uống 5mL",
+      "Gluco C viên nén ngậm",
+      "Rutin C viên nén",
+      "Upsa C-calcium viên nén sủi",
+      "Astymin forte và Astymin Liquid",
+      "Brivita (viên sủi vị cam)",
+      "Cebitex-C 300",
+      "Centovit",
+      "Enervon C",
+      "Homtamin",
+      "My Vita multivitamin viên sủi",
+      "Orange multivitamin viên sủi",
+      "Opssi multivitamin viên sủi",
+      "Plusssz multivitamin viên sủi",
+      "Ameflu + C",
+      "Efferalgan-vitamin C"
+    ],
+    "chu_y_khi_chi_dinh": {
+      "muc_do": 3,
+      "noi_dung": "Cân nhắc nguy cơ/lợi ích",
+      "thieu_hut_G6PD": "Đối với người bệnh thiếu hụt G6PD, có nguy cơ tan máu.",
+      "soi_than": "Tránh dùng vitamin C liều hàng ngày vượt quá 1g khi có sỏi thận (làm giảm pH ở thận)."
+    },
+    "tuong_tac": [
+      {
+        "thuoc": "Alopurinol hoặc dẫn chất",
+        "muc_do": 4,
+        "phan_tich": "Dùng acid ascorbic thường xuyên với liều cao làm acid hoá nước tiểu và có thể làm kết tủa tinh thể urat ở thận.",
+        "xu_ly": "Ở người bị gút, nên tránh làm acid hoá nước tiểu và thường được khuyến cáo dùng nước khoáng kiềm để tạo thuận lợi cho các tinh thể urat được hoà tan."
+      },
+      {
+        "thuoc": "Các chất kiềm hoá nước tiểu",
+        "muc_do": 4,
+        "phan_tich": "Acid ascorbic là thuốc acid hoá nước tiểu. Khi dùng các thuốc kiềm hoá nước tiểu, pH nước tiểu sẽ bị trung hoà khi dùng vitamin C liều cao (trên 2g).",
+        "xu_ly": "Nếu cần kiềm hoá nước tiểu, cần tránh phối hợp với vitamin C. Theo dõi pH nước tiểu bằng giấy thử acid-base."
+      },
+      {
+        "thuoc": "Sulfamid kháng khuẩn",
+        "muc_do": 4,
+        "phan_tich": "Nguy cơ kết tủa ở đường tiết niệu khi dùng vitamin C liều cao (trên 2g, tương tác lý - hoá về lý thuyết, cần phải chứng minh trên thực tế).",
+        "xu_ly": "Theo dõi pH nước tiểu. Nếu cần kiềm hoá nước tiểu, tránh kê đơn cùng với vitamin C. Thông báo cho người bệnh biết nguy cơ này và yêu cầu tạm ngừng dùng vitamin C liều cao trong khi điều trị bằng sulfamid."
+      },
+      {
+        "thuoc": "Barbituric",
+        "muc_do": 2,
+        "phan_tich": "Dùng đồng thời thuốc này với vitamin C sẽ tăng đào thải acid ascorbic.",
+        "xu_ly": "Nhu cầu vitamin C ở người bệnh điều trị lâu dài bằng barbituric tăng lên, nên có thể kê đơn bổ sung vitamin C."
+      },
+      {
+        "thuoc": "Deferoxamin",
+        "muc_do": 2,
+        "phan_tich": "Dùng acid ascorbic hàng ngày với liều cao hơn 250 mg cùng với deferoxamin có thể làm hư hại khả năng tạo phức của deferoxamin, từ đó làm tăng độc tính của sắt đối với mô, đặc biệt mô tim, có thể dẫn tới tim mất bù.",
+        "xu_ly": "Không kê đơn dùng đồng thời vitamin C đối với người bệnh điều trị bằng deferoxamin."
+      },
+      {
+        "thuoc": "Primidon hoặc dẫn chất",
+        "muc_do": 2,
+        "phan_tich": "Dùng các thuốc này đồng thời với acid ascorbic làm tăng thải acid ascorbic.",
+        "xu_ly": "Nhu cầu vitamin C ở người bệnh điều trị lâu dài bằng những thuốc này tăng lên, nên có thể bổ sung vitamin C."
+      },
+      {
+        "thuoc": "Thuốc chẹn beta (propranolol)",
+        "muc_do": 2,
+        "phan_tich": "Tác dụng dược lý của propranolol có thể bị giảm. Cơ chế có thể do giảm hấp thu propranolol.",
+        "xu_ly": "Giám sát đáp ứng lâm sàng của người bệnh và điều chỉnh liều khi cần."
+      },
+      {
+        "thuoc": "Fluphenazin",
+        "muc_do": 1,
+        "phan_tich": "Tác dụng dược lý và điều trị của fluphenazin có thể bị giảm. Tương tác xảy ra muộn. Cơ chế chưa rõ.",
+        "xu_ly": "Không cần phải xử lý. Giảm dần liều fluphenazin nếu cần."
+      },
+      {
+        "thuoc": "Thuốc uống ngừa thai",
+        "muc_do": 1,
+        "phan_tich": "Acid ascorbic làm tăng nồng độ trong huyết thanh của estrogen chứa trong viên thuốc, có thể gây ra các tác dụng không mong muốn.",
+        "xu_ly": "Nếu có tác dụng có hại xảy ra, có liên quan đến estrogen, nên nghĩ đến khả năng do phối hợp với acid ascorbic."
+      },
+      {
+        "thuoc": "Warfarin",
+        "muc_do": 1,
+        "phan_tich": "Tác dụng chống đông máu của warfarin có thể bị giảm. Cơ chế chưa rõ.",
+        "xu_ly": "Không cần can thiệp trừ khi người bệnh dùng một liều lớn (>5-10g/ ngày) acid ascorbic."
+      }
+    ]
+  },
+  {
+    "hoat_chat": "Acid chenodesoxycholic/ursodesoxycholic",
+    "mo_ta": "Acid này là thành phần cấu tạo bình thường của mật. Khi uống, thuốc có thể làm tan sỏi cholesterol ở túi mật với điều kiện là sỏi trong suốt với tia X (sỏi không vôi hoá).",
+    "cac_thuoc_trong_nhom": [
+      "ACID CHENODESOXYCHOLIC viên nén bọc 250mg",
+      "Chenodex viên nén bọc 250mg",
+      "ACID URSODESOXYCHOLIC nang 200mg",
+      "Ursolvan nang 200mg",
+      "Bio-clean (phối hợp) viên bao đường"
+    ],
+    "chu_y_khi_chi_dinh": {
+      "chong_chi_dinh": {
+        "muc_do": 4,
+        "noi_dung": "Chống chỉ định",
+        "thoi_ky_mang_thai": "Do đã thấy độc tính với gan của thai ở khỉ Rhesus.",
+        "benh_dai_trang": "Acid chenodesoxycholic dễ hấp thu ở ruột non. Có thể có phần không được hấp thu, sẽ gây bài tiết nước và các chất điện giải ở đại tràng. Khi đó thường xảy ra tiêu chảy, nên chống chỉ định dùng thuốc này ở những người có bệnh đại tràng. Tuy nhiên, acid ursodesoxycholic không kích ứng niêm mạc đại tràng nên có thể dùng được.",
+        "cac_truong_hop_khac": "Chống chỉ định dùng loại này khi có bệnh tổn thương thực thể ở ruột do nguy cơ tiêu chảy."
+      },
+      "can_nhac": {
+        "muc_do": 3,
+        "noi_dung": "Cân nhắc nguy cơ/lợi ích",
+        "suy_gan": "Sự tạo thành acid lithocholic do các vi khuẩn ở ruột có thể dẫn đến tăng vừa phải và nhất thời các transaminase."
+      },
+      "can_theo_doi": {
+        "muc_do": 1,
+        "thoi_ky_cho_con_bu": "Do thận trọng"
+      }
+    },
+    "tuong_tac": [
+      {
+        "thuoc": "Aminosid uống; cholestyramin; estrogen hoặc thuốc ngừa thai estroprogestogen; fibrat; progestogen hoặc dẫn chất; thuốc kháng acid đường uống hoặc than hoạt",
+        "muc_do": 3,
+        "phan_tich": "Acid ursochenodesoxycholic làm tăng tỷ lệ acid mật/ cholesterol. Khi lập lại cân bằng giữa các thành phần của mật, thuốc có thể góp phần làm thoái triển những sỏi mật cholesterol. Tạo phức với acid ursodesoxycholic (khi dùng cùng với cholestyramin và các kháng acid), hoặc làm tăng sự bão hoà của mật (khi dùng cùng với estrogen, progestogen, fibrat, neomycin uống) sẽ làm giảm hoạt tính của acid ursodesoxycholic.",
+        "xu_ly": "Điều trị bằng acid ursodesoxycholic phải dài ngày (từ 6 tháng đến 1 năm). Cần tránh không phối hợp với những thuốc làm giảm tác dụng của acid ursodesoxycholic (như cholestyramin, các kháng acid, estrogen, progestrogen, fibrat, neomycin uống), trừ khi thuốc phối hợp chỉ dùng trong thời gian rất ngắn để tránh thất bại trong điều trị."
+      },
+      {
+        "thuoc": "Ciclosporin",
+        "muc_do": 1,
+        "phan_tich": "Acid ursodesoxycholic làm tăng hấp thu và tăng nồng độ trong huyết thanh của ciclosporin ở một số người bệnh, nhưng không dự đoán trước được. Acid ursodesoxycholic có vẻ không tương tác với ciclosporin.",
+        "xu_ly": "Thông tin còn hạn chế. Cần thận trọng giám sát tác dụng khi thêm hoặc ngừng acid ursodesoxycholic ở người đang điều trị bằng ciclosporin, để điều chỉnh liều ciclosporin khi cần."
+      },
+      {
+        "thuoc": "Ciprofloxacin",
+        "muc_do": 1,
+        "phan_tich": "Nồng độ trong huyết thanh của ciprofloxacin có thể bị giảm khi phối hợp với acid ursodesoxycholic. Cơ chế chưa rõ.",
+        "xu_ly": "Tuy thông tin còn ít (một trường hợp được báo cáo), nhưng cần phải giám sát tương tác này với bất cứ một kháng sinh quinolon nào khi phối hợp."
+      }
+    ]
   }
 ];
